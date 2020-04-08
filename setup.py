ec6512b5b91e63d5140d50d9c6f8e5a797689878
teaxu <yxucb@connect.ust.hk>
2020-04-09 01:05:35 +0800

    fix bug

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="caireCovid",
-    version="0.1.2",
+    version="0.1.4",
     author="Yan Xu",
     author_email="yxucb1229@gmail.com",
     description="system for covid-19.",
